e53d843f07b3dd0ef1f4b87e69048732e30ebfcb
leonardo moreira <leonardo.moreira@fundepes.br>
2025-12-05 21:12:08 -0300

    final estrutura

--- src/main/java/br/com/tormenta20/model/Atributos.java
+++ src/main/java/br/com/tormenta20/model/Atributos.java
@@ -1,5 +1,9 @@
+package main.java.br.com.tormenta20.model;
+
 public class Atributos {
+
     private int forca, destreza, constituicao, inteligencia, sabedoria, carisma;
+
     public Atributos(int FOR,int DES,int CON,int INT,int SAB,int CAR){
         this.forca = FOR; this.destreza = DES; this.constituicao = CON;
         this.inteligencia = INT; this.sabedoria = SAB; this.carisma = CAR;
--- src/main/java/br/com/tormenta20/model/ElementoFicha.java
+++ src/main/java/br/com/tormenta20/model/ElementoFicha.java
@@ -1,3 +1,6 @@
-public class Ficha {
-    
+package main.java.br.com.tormenta20.model;
+import br.com.tormenta20.interfaces.IDescritivel;
+
+public abstract class ElementoFicha implements IDescritivel {
+
 }
